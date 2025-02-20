5d349e3aeaed6c8b14add61c0cdf83a239fed114
Michael Ge <56697279+Spitfier720@users.noreply.github.com>
2025-02-19 19:17:48 -0500

    Implemented Searching Functionality

--- cooking-screwups/src/App.js
+++ cooking-screwups/src/App.js
@@ -5,12 +5,23 @@ import Table from './Table';
 
 function App() {
   const [entries, setEntries] = useState([]);
+  const [filteredEntries, setFilteredEntries] = useState([]);
 
   useEffect(() => {
     if(process.env.REACT_APP_USE_PERSONAL_DATA === 'true') {
       setEntries(screwupLog);
+      setFilteredEntries(screwupLog);
     }
-  }, [])
+  }, []);
+  
+  const handleSearchChange = (e) => {
+    const query = e.target.value;
+    setFilteredEntries(
+      entries.filter(
+        entry => entry.dish.toLowerCase().includes(query.toLowerCase())
+      )
+    );
+  }
 
   return (
     <div className="App">
@@ -18,10 +29,14 @@ function App() {
         <title>Cooking Screwups</title>
         <h1>Cooking Screwups</h1>
         <span>
-          <input type="text" placeholder="Type a dish"></input>
+          <input 
+            type="text" 
+            placeholder="Type a dish"
+            onChange = {handleSearchChange}
+          />
           <button>Add Screwup</button>
         </span>
-        <Table entries={entries} />
+        <Table entries={filteredEntries} />
       </header>
     </div>
   );
